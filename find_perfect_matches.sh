32471742a26edbdab91cf9598eac44a8e74f1aba
khimaniasma <khimaniasma@gmail.com>
2023-09-11 17:19:38 -0400

    Q10 print

--- find_perfect_matches.sh
+++ find_perfect_matches.sh
@@ -8,5 +8,3 @@ tblastn -query $1 -subject $2 -outfmt '6 std qlen' -out $3
 awk -F"\t" '{if ($3>30 && $4>(0.9*$13)) > $3) print}' $3 >  new_matches
 count=$(cat new_matches | wc -l)
 echo "there are $count matches"
-
-
